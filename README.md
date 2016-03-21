4e55e84153462d17dc787e8d199ae9d40a3ca87b
Sean "These Are Not the Changes You Are Looking For" Waite <swaite@redhat.com>
2016-03-20 22:38:53 -0400

    db.py update, make the README a little more useful
    Signed-off-by: Sean "These Are Not the Changes You Are Looking For" Waite <swaite@redhat.com>

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-This is for ideas and maybe eventually code
-for a simple linux amateur radio contest logger.
+Clogger: The Gentleman's Contest Logger
+
+This is a contest logger, initially developed for Linux
+but may end up on other OS' in the future.
+
+Currently it's being developed for the ARRL VHF+ contest,
+but the intent is to expand and make it useful for all
+contests in general.
--- db.py
+++ db.py
@@ -1,54 +1,74 @@
 import sqlite3
 
 class contestDB:
-  def createDB():
-    sqlite_file = '/home/swaite/radio/testdb'
-    
-    conn = sqlite3.connect(sqlite_file)
+  sqlite_file = ''
+  qsoTable = ''
+  contestTable = ''
+
+  def __init__(self):
+    self.sqlite_file = '/home/swaite/radio/testdb'
+    #qsotable is for data on a contact
+    self.qsoTable = 'qso_table'
+    #contestTable is for contest meta
+    self.contestTable = 'contest_table'
+
+  def createDB(self):
+
+    conn = sqlite3.connect(self.sqlite_file)
 
     #create the tables
-    #qsotable is for data on a contact
-    qsoTable = 'qso_table'
-    #contestTable is for contest meta
-    contestTable = 'contest_table'
     
     c = conn.cursor()
 
     c.execute("CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)"\
-      .format(tn=qsoTable, nf='qso_id', ft='INTEGER'))
+      .format(tn=self.qsoTable, nf='qso_id', ft='INTEGER'))
 
     c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-      .format(tn=qsoTable, cn='their_call', ct='TEXT'))
+      .format(tn=self.qsoTable, cn='their_call', ct='TEXT'))
 
     c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-      .format(tn=qsoTable, cn='operator', ct='TEXT'))
+      .format(tn=self.qsoTable, cn='operator', ct='TEXT'))
     
     c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-      .format(tn=qsoTable, cn='frequency', ct='REAL'))
+      .format(tn=self.qsoTable, cn='frequency', ct='REAL'))
     
     c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-      .format(tn=qsoTable, cn='band', ct='TEXT'))
+      .format(tn=self.qsoTable, cn='band', ct='TEXT'))
     
     c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-      .format(tn=qsoTable, cn='mode', ct='TEXT'))
+      .format(tn=self.qsoTable, cn='mode', ct='TEXT'))
     
     c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-      .format(tn=qsoTable, cn='exchange', ct='TEXT'))
+      .format(tn=self.qsoTable, cn='exchange', ct='TEXT'))
     
     c.execute("CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)"\
-      .format(tn=contestTable, nf='callsign', ft='TEXT'))
+      .format(tn=self.contestTable, nf='callsign', ft='TEXT'))
     
     c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-      .format(tn=contestTable, cn='club', ct='TEXT'))
+      .format(tn=self.contestTable, cn='club', ct='TEXT'))
     
     c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-      .format(tn=contestTable, cn='contest', ct='TEXT'))
+      .format(tn=self.contestTable, cn='contest', ct='TEXT'))
     
     c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-      .format(tn=contestTable, cn='soapbox', ct='BLOB'))
+      .format(tn=self.contestTable, cn='soapbox', ct='BLOB'))
     
     c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-      .format(tn=contestTable, cn='category', ct='TEXT'))
+      .format(tn=self.contestTable, cn='category', ct='TEXT'))
 
     conn.commit()
     conn.close()
+
+  def addQSO(self, their_call, operator, frequency, band, mode, exchange):
+    print "nope"
+    conn = sqlite3.connect(self.sqlite_file)
+    
+    c = conn.cursor()
+
+    c.execute("INSERT INTO {tn} (qso_id, their_call, operator, frequency, \
+      band, mode, exchange) VALUES(3, {them}, {me}, {freq}, {band}, {mode},\
+      {exch})".format(tn=self.qsoTable,them=their_call, me=operator, freq=frequency, \
+      band=band, mode=mode, exch=exchange))
+
+    conn.commit()
+    conn.close()  
